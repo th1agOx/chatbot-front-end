2f9e59ff9e261a946a6898dcce1c0cef87a86c0e
JpMazzotti <mazzotti0202@gmail.com>
2026-06-29 17:45:01 -0300

    versao 2 base

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,8 @@ export const MAX_FILE_SIZE_BYTES = 10 * 1024 * 1024
 
 export const ALLOWED_FILE_TYPES = ['txt', 'pdf'] as const
 
+export const AUTH_TOKEN_KEY = 'chatbot_token'
+
 export const ACCEPTED_FILE_MIME_TYPES = {
   txt: '.txt',
   pdf: '.pdf',
